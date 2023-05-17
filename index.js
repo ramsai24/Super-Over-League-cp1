5dd297f2249dd5f61eb5c462d3c2256e3e66a923
ramsai24 <jnrsai24@gmail.com>
2023-05-17 13:16:52 +0000

    render items unable to view

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const element = (
+//   Write your code here.
+
+    const bgContainer = React.createElement("div", {className:"bg-container"})
+
+    const h1Element = React.createElement("h1",{className:"title",children:"Super Over League"})
+
+
+)
+
+ReactDOM.render(element, document.getElementById('root'))
